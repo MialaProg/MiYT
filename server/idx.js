cb4969032361e55548bb762c769e432af0a8d7b3
Miala <65893041+Miala-python@users.noreply.github.com>
2024-02-15 19:49:12 +0100

    ..2

--- server/idx.js
+++ server/idx.js
@@ -1,4 +1,4 @@
-console.log('SrvIdx ID-01');
+console.log('SrvIdx ID-02');
 
 function setYouTubePlDt(PlID) {
     let apiUrl = 'https://www.youtube.com/oembed?url=https://www.youtube.com/watch?v=' + PlID + '&format=json';
@@ -21,59 +21,62 @@ function setYouTubePlDt(PlID) {
 
 function ListPl() {
    
-    
-let playlists = document.getElementById("listoflist").innerHTML.split("¤*¤");
-
-let pl_view_HTML = `
-<div class="card has-background-info-dark has-text-primary-light">
-<header class="card-header">
-    <p class="card-header-title">
-    Vidéos de la liste de lecture (Beta)  
-    </p>
-</header>
-<div class="card-content">
-    <div class="content"><ol>
-`;
-
-let i = 0;
-playlists.forEach((pl, index) => {
-    if (index % 2 === 1) {
-        let name = playlists[index - 1];
-        let link = 'https://miala.000webhostapp.com/YT/www.php?list=' + pl + '&title=' + name;
-        // pl_view_HTML += '<li><a href="' + link + '">' + name + '</a></li>';
         
-        pl_view_HTML += `
-        <li><a id="pl_view_${i}" class="has-text-success-light" href="${link}">
-            <article id="pl_view_article_${i}" class="media video_selection">
-                <figure class="media-left" style="width: 7rem">
-                    <p class="image is-4by3">
-                    <img id="pl-img-${pl}" src="https://i.ytimg.com/vi/${pl}/hqdefault.jpg">
-                    </p>
-                </figure>
-                <div class="media-content">
-                    <div class="content">
-                    <p id=pl-txt-${pl}">
-                    ${name}
-                    <small>#${i} : ${pl}</small>
-                    </p>
+    let playlists = document.getElementById("listoflist").innerHTML.split("¤*¤");
+
+    let pl_view_HTML = `
+    <div class="card has-background-info-dark has-text-primary-light">
+    <header class="card-header">
+        <p class="card-header-title">
+        Vidéos de la liste de lecture (Beta)  
+        </p>
+    </header>
+    <div class="card-content">
+        <div class="content"><ol>
+    `;
+
+    playlists.forEach((pl, i) => {
+        if (i % 2 === 1) {
+            let name = playlists[i - 1];
+            let link = 'https://miala.000webhostapp.com/YT/www.php?list=' + pl + '&title=' + name;
+            // pl_view_HTML += '<li><a href="' + link + '">' + name + '</a></li>';
+            
+            pl_view_HTML += `
+            <li><a id="pl_view_${i}" class="has-text-success-light" href="${link}">
+                <article id="pl_view_article_${i}" class="media video_selection">
+                    <figure class="media-left" style="width: 7rem">
+                        <p class="image is-4by3">
+                        <img id="pl-img-${pl}" src="https://i.ytimg.com/vi/${pl}/hqdefault.jpg">
+                        </p>
+                    </figure>
+                    <div class="media-content">
+                        <div class="content">
+                        <p id=pl-txt-${pl}">
+                        ${name}
+                        <small>#${i} : ${pl}</small>
+                        </p>
+                        </div>
                     </div>
-                </div>
-            </article>
-        </a></li>`;
-    }
-    i += 1;
-});
+                </article>
+            </a></li>`;
+        }
+    });
 
 
-pl_view_HTML += `</ol>
-</div>
-</div>
-</div>
-</div>`;
+    pl_view_HTML += `</ol>
+    </div>
+    </div>
+    </div>
+    </div>`;
 
-var pl_view = document.getElementById("playlists");
-pl_view.innerHTML = pl_view_HTML;
+    var pl_view = document.getElementById("playlists");
+    pl_view.innerHTML = pl_view_HTML;
 
+    playlists.forEach((pl, index) => {
+        if (index % 2 === 1) {
+            setYouTubePlDt(pl);
+        }
+    });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
