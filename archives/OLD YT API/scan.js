6a396dc321601934754d6d13ddd7db856d5c380a
Miala <65893041+Miala-python@users.noreply.github.com>
2024-03-16 20:03:22 +0100

    del

--- archives/OLD YT API/scan.js
+++
@@ -1,5 +0,0 @@
-console.log('ScanMi ID03');
-
-
-window.location.href = 'https://yt.mi.42web.io/www.php?list=' + new URLSearchParams(new URL(window.location.href).search).get("list") + '&title=' + document.title;
-
